57ebf01bce95a25cfe3151c7b1da4538e1df0db1
Helloyunho <yunho050840@gmail.com>
2024-08-21 12:53:57 +0900

    Initial Commit

---
+++ DRS Chart Editor/DRS_Chart_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DRS_Chart_EditorApp.swift
+//  DRS Chart Editor
+//
+//  Created by Helloyunho on 2024/8/21.
+//
+
+import SwiftUI
+
+@main
+struct DRS_Chart_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
